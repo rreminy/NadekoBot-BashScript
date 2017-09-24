0486f6faf3dea8444dfd063cf97bed7efd021f86
AzureGem <rreminy@gmail.com>
2017-09-24 19:52:49 -0400

    Update nadeko_installer.sh
    changing stuff

--- nadeko_installer.sh
+++ nadeko_installer.sh
@@ -25,7 +25,7 @@ mkdir "$tempdir"
 cd "$tempdir"
 
 echo "Downloading NadekoBot, please wait."
-git clone -b $1 --recursive --depth 1 https://github.com/rreminy/NadekoBot.git
+git clone -b $1 --recursive --depth 1 https://github.com/Kwoth/NadekoBot.git
 echo "NadekoBot downloaded."
 echo ""
 
